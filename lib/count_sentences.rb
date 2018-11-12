1a6bee2465f89b63952cb267ed6a83d807b78025
a lan <bluenowte97@gmail.com>
2018-11-12 16:12:50 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,25 +4,18 @@ class String
 
   def sentence?
     self.end_with?(".")
-
   end
-
   def question?
     self.end_with?("?")
-
   end
-
   def exclamation?
     self.end_with?("!")
   end
-
   def count_sentences
     count = 0
     item = []
-
     item = self.split(/[.*!*?]/)
     item.reject! { |element| element.nil? || element == '' }
-    puts item
     count = item.length
   end
 end
